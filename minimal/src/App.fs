cf46b1ec416062c5658835e58c99511d67bb770f
Martin <martin.vanleeuwen@tno.nl>
2019-10-15 17:11:31 +0200

    converting

--- minimal/src/App.fs
+++ minimal/src/App.fs
@@ -6,20 +6,36 @@ open Fable.Import
 open Fable.Import.Browser
 
 open Fable.Core
-let mutable myCanvas : Browser.HTMLCanvasElement = unbox document.getElementById "myCanvas"  // myCanvas is defined in public/index.html
+open Fable.PowerPack.Fetch
+open Fable.PowerPack
+//let mutable myCanvas : Browser.HTMLCanvasElement = unbox document.getElementById "myCanvas"  // myCanvas is defined in public/index.html
 
-type Song =
-    abstract GetCurrentTime : unit -> float
-    abstract Duration : unit -> float
-type ISongPlayer = 
-    abstract PlaySong : string -> int -> int -> Song
+// type Song =
+//     abstract GetCurrentTime : unit -> float
+//     abstract Duration : unit -> float
+// type ISongPlayer = 
+//     abstract PlaySong : string -> int -> int -> Song
+document.write("Print")
+console.log("yo")
+// let onSongSelected = new Event<string>()|> fun x -> x.Publish
+// onSongSelected.Add(console.log)
 
 
-[<Import("*", "./song.js")>]
-let songPlayer : ISongPlayer = jsNative
+let x = fetch "http://fable.io" []
+        |> Promise.bind (fun res -> res.text())
+        |> Promise.map (console.log)
 
-let song = songPlayer.PlaySong "../music/lessons.mp3" 0 1
-let rec main(x) =
-    console.log(song.GetCurrentTime())
-    window.requestAnimationFrame main |> ignore
-main(0.0)
+
+// [<Import("*", "./song.js")>]
+// let songPlayer : ISongPlayer = jsNative
+
+// let song = songPlayer.PlaySong "../music/lessons.mp3" 0 1
+// let rec main(x) =
+//     console.log(song.GetCurrentTime())
+//     window.requestAnimationFrame main |> ignore
+
+// main(0.0)
+
+
+
+
--- minimal/src/loader.fs
+++ minimal/src/loader.fs
@@ -1,26 +1,86 @@
 module Loader
+open Fable.PowerPack
+open Fable.PowerPack.Fetch
+open Fable.Import
+open Fable.Import.Browser
 
-type Time = float
-type Frequency = float
-type Intensity = float
-type IsBuildUp = bool
+let loadfile file callback = 
+        fetch file []
+        |> Promise.bind (fun res -> res.text())
+        |> Promise.map callback
+
+let convertToNumberArray(line: string) =
+    line.Split ','|>Array.map(float)   
+
+type Onset = {
+    time:float;
+    frequency:float;
+    IsBuildUp:bool
+}
+type Energy = {
+    time:float;
+    energy:float;
+}
+type SongData= {
+    energies: Energy array;
+    onsets : Onset array
+    maxFrequencies : int
+}
+
+let createSongData (t_onset: float array) (f_onset: float array) 
+    (e: float array) (buildupOnsets: float array)
+    (buildUpEnergies : float array) 
+    (duration : float) = 
+    let integerBuildupOnsets = buildupOnsets|>Array.map int
+    let dt = duration / (float e.Length)
+    let onsets = Array.mapi2 (fun i t f  -> 
+            {time=t;frequency=f;IsBuildUp = Array.contains i integerBuildupOnsets}) t_onset f_onset
+    let energies = Array.mapi (fun i en  -> {time=(float i)*dt;energy =en}) e
+    {energies=energies;onsets=onsets;maxFrequencies = 11}
 
 
-type Onset = Time*Frequency*IsBuildUp
-type Energy = Time*Frequency
+type SelectedSong = {
+    source : string
+    loadSongData:float -> unit
+}
+
+    
+
+let selectSong idx  onSongSelected onSongDataLoaded =
+    console.log(availableSongs)
+
+    //if (idx==-1) {
+    //    idx = Math.floor(Math.random() * availableSongs.length)
+    //}
+    let furtherPr
+    let chosenSong = availableSongs.[idx] // availableSongs[Math.floor(Math.random() * availableSongs.length)]//
+    let loadSongData duration  =
+        let data = loadFile("../data/songs/" + chosenSong.slice(0, chosenSong.length - 4) + ".txt")
+        
+        //.split(/\r?\n/).map(convertToNumberArray)
+        let songdata : SongData = new SongData(data.[0],data.[1],data.[2],data.[3],data.[4],duration)
+ //       onSongDataLoaded.fire(songdata)
+        ()
+    loadfile ("../data/available.txt".split '\n') (fun x -> console.log(x))|> ignore
+        
+    
+    ()
+//    onSongSelected.fire({source:chosenSong,loadSongData:loadSongData})
 
 
-let loadFile src = 
+
+
+// let loadFile src = 
     
 
 
-let selectSong ?idx =
-    var availableSongs = loadFile("../data/available.txt").split(/\r?\n/)
-    availableSongs.pop()
-    console.log(availableSongs)
-    if (idx===-1) {
-        idx = Math.floor(Math.random() * availableSongs.length)
-    }
-    var chosenSong = availableSongs[idx] // availableSongs[Math.floor(Math.random() * availableSongs.length)]//
-    return chosenSong.slice(0, chosenSong.length - 4) + ".mp3"
+// let selectSong ?idx =
+//     var availableSongs = loadFile("../data/available.txt").split(/\r?\n/)
+//     availableSongs.pop()
+//     console.log(availableSongs)
+//     if (idx===-1) {
+//         idx = Math.floor(Math.random() * availableSongs.length)
+//     }
+//     var chosenSong = availableSongs[idx] // availableSongs[Math.floor(Math.random() * availableSongs.length)]//
+//     return chosenSong.slice(0, chosenSong.length - 4) + ".mp3"
 
